49f1ffc14b795892a03382ea8cf33fa01e4fda13
bojpop <poppfc@gmail.com>
2017-10-16 00:16:22 +0200

    Employee

---
+++ EmployeeService/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace EmployeeService
+{
+    [DataContract]
+    public class Employee
+    {
+        private int _id;
+        private string _name;
+        private string _gender;
+        private DateTime _dateOfBirth;
+
+        [DataMember]
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        [DataMember]
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        [DataMember]
+        public string Gender
+        {
+            get { return _gender; }
+            set { _gender = value; }
+        }
+
+        [DataMember]
+        public DateTime DateOfBirth
+        {
+            get { return _dateOfBirth; }
+            set { _dateOfBirth = value; }
+        }
+    }
+}
