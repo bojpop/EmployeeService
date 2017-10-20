f6853a8f4014b15913eabe9c81c442c6ed5a8006
POSTA\bojan.m.popovic <bojan.m.popovic@posta.rs>
2017-10-20 15:25:14 +0200

    MessageContract

--- EmployeeService/Employee.cs
+++ EmployeeService/Employee.cs
@@ -4,9 +4,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.Serialization;
+using System.ServiceModel;
 
 namespace EmployeeService
 {
+    [MessageContract(IsWrapped = true, WrapperName = "EmployeeRequestObject", WrapperNamespace = "http://MyCompany.com/Employee")]
+    public class EmployeeRequest
+    {
+        [MessageHeader(Namespace = "http://MyCompany.com/Employee")]
+        public string LicenseKey { get; set; }
+
+        [MessageBodyMember(Namespace = "http://MyCompany.com/Employee")]
+        public int EmployeeId { get; set; }
+    }
+
+    [MessageContract(IsWrapped = true, WrapperName = "EmployeeInfoObject", WrapperNamespace = "http://MyCompany.com/Employee")]
+    public class EmployeeInfo
+    {
+        public EmployeeInfo()
+        {
+            
+        }
+
+        public EmployeeInfo(Employee employee)
+        {
+            
+        }
+
+        [MessageBodyMember(Order = 1, Namespace = "http://MyCompany.com/Employee")]
+        public int Id { get; set; }
+        [MessageBodyMember(Order = 2, Namespace = "http://MyCompany.com/Employee")]
+        public string Name { get; set; }
+        [MessageBodyMember(Order = 3, Namespace = "http://MyCompany.com/Employee")]
+        public string Gender { get; set; }
+        [MessageBodyMember(Order = 4, Namespace = "http://MyCompany.com/Employee")]
+        public DateTime DOB { get; set; }
+        [MessageBodyMember(Order = 5, Namespace = "http://MyCompany.com/Employee")]
+        public Employee.EmployeeType Type { get; set; }
+        [MessageBodyMember(Order = 6, Namespace = "http://MyCompany.com/Employee")]
+        public int AnnualSalary { get; set; }
+        [MessageBodyMember(Order = 7, Namespace = "http://MyCompany.com/Employee")]
+        public int HourlyPay { get; set; }
+        [MessageBodyMember(Order = 8, Namespace = "http://MyCompany.com/Employee")]
+        public int HoursWorked { get; set; }
+    }
+
     //[KnownType(typeof(FullTimeEmployee))]
     //[KnownType(typeof(PartTimeEmployee))]
     [DataContract]
